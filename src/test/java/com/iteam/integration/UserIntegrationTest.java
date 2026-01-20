cedfd91edb62d683feac0b13cb5916969abcfcdc
wajdi ben ameur <ben.ameur.wajdi@gmail.com>
2026-01-20 13:57:06 +0100

    rectif

--- src/test/java/com/iteam/integration/UserIntegrationTest.java
+++ src/test/java/com/iteam/integration/UserIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Transactional
 class UserIntegrationTest {
-
+//tests
     @Autowired
     private MockMvc mockMvc;
 
